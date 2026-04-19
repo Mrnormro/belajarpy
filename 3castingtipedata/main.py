fefccf501d62342ea29c035ca171a4ae903157c8
Mrnormro <morenormro@gmail.com>
2026-04-20 05:38:25 +0700

    materi casting tipe data

---
+++ 3castingtipedata/main.py
@@ -0,0 +1,11 @@
+#merubah tipe data ke tipe lain
+
+data_int = 9
+print("data =",data_int,"type =",type(data_int))
+
+data_float = float(data_int)
+data_str = str(data_int)
+data_bool = bool(data_int)
+print("data =",data_float,"type =",type(data_float))
+print("data =",data_str,"type =",type(data_str))
+print("data =",data_bool,"type =",type(data_bool)) # hanya false jika nilai int 0
