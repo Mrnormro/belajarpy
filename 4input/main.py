de509cda0cefe10af7540c4d7b5d01c6450c8c99
Mrnormro <morenormro@gmail.com>
2026-04-20 05:50:19 +0700

    materi input

---
+++ 4input/main.py
@@ -0,0 +1,16 @@
+
+# # data yang dimasukkan pasti string
+# data = input("input masukkan data : ")
+# print("data =",data,"type =",type(data))
+
+# # input integer
+# data_int = int(input("masukkan angka : "))
+# print("data =",data_int,"type =",type(data_int))
+
+# #input float
+# data_float = float(input("masukkan angka : "))
+# print("data =",data_float,"type =",type(data_float))
+
+# input bool/biner
+data_bool = bool(int(input("masukkan nilai bool : ")))
+print("data =",data_bool,"type =",type(data_bool))
