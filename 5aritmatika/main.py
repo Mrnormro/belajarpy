c8e16e4d0decd411ae9edab1a8f67bd58b1bf2a7
Mrnormro <morenormro@gmail.com>
2026-04-21 15:39:28 +0700

    materi operator aritmatika

---
+++ 5aritmatika/main.py
@@ -0,0 +1,30 @@
+a = 10
+b = 3
+
+# operasi penjumlahan
+hasil = a + b
+print(a,"+",b,"=",hasil)
+
+# operasi pengurangan
+hasil = a - b
+print(a,"-",b,"=",hasil)
+
+# operasi perkalian
+hasil = a * b
+print(a,"*",b,"=",hasil)
+
+# operasi pembagian
+hasil = a / b
+print(a,"/",b,"=",hasil)
+
+# operasi eksponen / pangkat
+hasil = a ** b
+print(a,"**",b,"=",hasil)
+
+# operasi sisa bagi/modulus
+hasil = a % b
+print(a,"%",b,"=",hasil)
+
+# operasi floor division
+hasil = a // b
+print(a,"//",b,"=",hasil)
