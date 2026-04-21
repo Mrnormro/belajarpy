f26fd2991a52d7cabd05f931ead756a1cdb26d24
Mrnormro <morenormro@gmail.com>
2026-04-21 20:13:50 +0700

    materi assigment

---
+++ 11assignment/main.py
@@ -0,0 +1,74 @@
+# =
+a = 10
+print(a)
+
+# +=
+b = 10
+b += 3
+print(b)
+
+# -=
+c = 10
+c -= 3
+print(c)
+
+# *=
+d = 10
+d *= 3
+print(d)
+
+# /=
+b
+e= 10
+e /= 3
+print(e)
+
+# %=
+f = 10
+f %= 3
+print(f)
+
+# //=
+g = 10
+g //= 3
+print(g)
+
+# **=
+h = 10
+h **= 3
+print(h)
+
+# &=
+i = 10
+i &= 3
+print(i)
+
+# |=
+j = 10
+j |= 3
+print(j)
+
+# ^=
+k = 10
+k ^= 3
+print(k)
+
+# >>=
+l = 10
+l >>= 3
+print(l)
+
+# <<=
+m = 10
+m <<= 3
+print(m)
+
+
+
+
+
+
+
+
+
+
