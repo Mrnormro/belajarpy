dcc1df61e41b031a16e9944e926bada28884a0e6
Mrnormro <morenormro@gmail.com>
2026-04-21 18:34:10 +0700

    materi operasi logika

---
+++ 8operasilogika/main.py
@@ -0,0 +1,30 @@
+#not, or, and, xor
+
+#not, kebalikan dari nilai yang ada
+a = True
+c = not a
+print(c)
+print()
+
+#or, nilai true, jika salah satu ada kedua nilai true
+a = True
+b = True
+c = a or b
+print(c)
+print()
+
+#and, nilai true, jika kedua nilai true
+a = True
+b = True
+c = a and b
+print(c)
+print()
+
+#xor, nilai true, jika salah satu nilai true
+a = True
+b = False
+c = a ^ b
+print(c)
+print()
+
+
