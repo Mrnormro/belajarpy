b05eeb4dc5b6bcc34fab32c0dce1856015807aee
Mrnormro <morenormro@gmail.com>
2026-04-22 11:34:02 +0700

    pendahuluan string

---
+++ 12string/main.py
@@ -0,0 +1,26 @@
+# 1. cara buat string
+data = "ini adalah string"
+print(data)
+print(type(data))
+
+# 2. menggunakan \ untuk simbol
+print('hari ini adalah hari jum\'at')
+print("C:\\USER\\UCUP")
+print("ucup\totong")
+print("baris 1\nbaris 2")
+
+# 3. menggunakan raw string
+print(r'C:\\USER\\UCUP')
+
+# 4. menggunakan literal string
+print("""
+nama : ucup
+hobi : makan
+""")
+
+# 5. multi literal string dan raw
+print(r"""
+nama : ucup
+hobi : makan
+folder foto : \foto\ucup.png
+""")
