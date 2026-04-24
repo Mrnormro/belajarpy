8005bbe3034285d0759dc417d140ded73f36ad16
Mrnormro <morenormro@gmail.com>
2026-04-24 12:59:48 +0700

    materi if dan else

---
+++ 18ifdanelse/main.py
@@ -0,0 +1,17 @@
+# if dan else
+
+# 1. if inline
+# nama = input("masukkan nama : ")
+# if nama=="ucup": print("kemana aja cup?")
+
+# 2. if indentation
+# nama = input("masukkan nama : ")
+# if nama=="ucup":
+#     print("kemana aja cup?")
+
+# 3. else statement
+nama = input("masukkan nama : ")
+if nama=="ucup":
+    print("kemana aja cup?")
+else:
+    print("lau sape?")
