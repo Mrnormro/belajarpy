41ccd80df434c6afc281611b8d969e826ec21605
Mrnormro <morenormro@gmail.com>
2026-04-26 18:14:45 +0700

    materi for loop

---
+++ 21forloop/main.py
@@ -0,0 +1,18 @@
+# menggunakan list
+listangka = [1,2,4,2,4]
+for i in listangka:
+    print(i)
+
+print()
+# menggunakan range
+for i in range(5):
+    print(i)
+
+
+for i in range(1,5):
+    print(i)
+
+# menggunakan string
+data = "Ucup Surucup"
+for i in data:
+    print(i)
