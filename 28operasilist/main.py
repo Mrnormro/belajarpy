3bab032991ad8f02315be7002f3700c8ee9a4438
Mrnormro <morenormro@gmail.com>
2026-04-29 20:01:53 +0700

    operasi list

---
+++ 28operasilist/main.py
@@ -0,0 +1,25 @@
+data_angka = [8,6,4,6,5,4,4,4,2,4]
+print(data_angka)
+
+# count data
+
+jumlah_4 = data_angka.count(4)
+print(jumlah_4)
+
+# ambil posisi data (index)
+data = ["hayam","opet","turit"]
+print(data)
+
+opet = data.index("opet")
+print(opet)
+
+# mengurutkan list
+print(data_angka)
+data_angka.sort()
+print(data_angka)
+
+# balik isi
+data.reverse()
+data_angka.reverse()
+print(data)
+print(data_angka)
