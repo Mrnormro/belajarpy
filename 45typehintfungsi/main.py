ac75ad883b1082002b4320a3cb8f872aa019a337
Mrnormro <morenormro@gmail.com>
2026-05-02 16:09:15 +0700

    type hint fungsi

---
+++ 45typehintfungsi/main.py
@@ -0,0 +1,15 @@
+# bentuk standar
+# def fungsi(parameter):
+#     print(parameter)
+
+# fungsi(1)
+# fungsi("ucup")
+# fungsi(True)
+
+# penggunaan type hint
+
+def pangkatsepuluh(argumrnt:int) -> int:
+    hasil = argumrnt**10
+    return hasil
+
+print(pangkatsepuluh(2))
