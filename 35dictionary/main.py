7c39fab5998f9299f35287d0161332260dcd58c8
Mrnormro <morenormro@gmail.com>
2026-05-02 10:54:55 +0700

    materi dictionary

---
+++ 35dictionary/main.py
@@ -0,0 +1,18 @@
+# list -> array, mengakses dengan menggunakan index
+
+data_list = ["ucup","otong","udin"]
+print(data_list[0])
+
+# dictionary (dict) -> associative array
+# identifier -> key
+
+data_dict = {
+    'cup':'ucup',
+    'tong':'otong',
+    'din':'udin',
+    'no':12,
+    'list':data_list
+}
+
+print(data_dict['cup'])
+print(data_dict['list'])
