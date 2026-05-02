11d2642c60898b6a1067d56139d0708324929084
Mrnormro <morenormro@gmail.com>
2026-05-02 20:19:04 +0700

    membuat module

---
+++ 51module/main.py
@@ -0,0 +1,10 @@
+# module matematika dengan import
+
+from matematika import tambah
+from matematika import kali as odd
+
+hasil_tambah = tambah(1,2,3,4)
+print(hasil_tambah)
+
+hasil_kali = odd(10,2)
+print(hasil_tambah)
